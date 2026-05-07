6366c187e8e89febdca42a8397f8dc0f5660a58f
geetashri8 <geetashriyadav23@navgurukul.org>
2026-05-07 14:34:07 +0530

    refactored tracker

--- app/(authenticated)/tracker/_components/index.ts
+++
@@ -1,6 +0,0 @@
-export { ActivityDatePicker } from "./ActivityDatePicker";
-export { FormHeader } from "./FormHeader";
-export { LifelinesCard } from "./LifelinesCard";
-export { LifelinesInfoButton } from "./LifelinesInfoButton";
-export { ProjectActivitiesSection } from "./ProjectActivitiesSection";
-export { ProjectEntryCard } from "./ProjectEntryCard";
